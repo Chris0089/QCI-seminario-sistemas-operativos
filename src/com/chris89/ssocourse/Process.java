fd993ba2d22858ca71587583fed6a400b54e050f
Christian Amparo <docs.chris89@gmail.com>
2019-03-28 16:07:18 -0600

    added Process Class methods

--- src/com/chris89/ssocourse/Process.java
+++ src/com/chris89/ssocourse/Process.java
@@ -1,4 +1,45 @@
 package com.chris89.ssocourse;
 
-public class Process {
+class Process {
+    private String name;
+    private int executionTime;
+    private int id;
+    private String castedName;
+    private int remainingTime;
+
+    public Process(String newName, int newTime){
+        name = newName;
+        executionTime = newTime;
+        remainingTime = newTime;
+        generateCastedName();
+    }
+    public Process(String newName, int newTime, int newId){
+       this(newName, newTime);
+       id = newId;
+    }
+    private void generateCastedName(){
+        castedName = name + " (" + Integer.toString(remainingTime) + ")";
+    }
+    String getCastedName(){
+        if(castedName==""){
+            generateCastedName();
+            return castedName;
+        }else{
+            return castedName;
+        }
+    }
+    void updateRemainingTime(int newTime){
+        remainingTime = newTime;
+    }
+    void decRemeaningTime(){
+        --remainingTime;
+    }
+    void resetRemaingTime(){
+        remainingTime = executionTime;
+    }
+    void updateId(int newID){
+        id=newID;
+    }
+
+
 }
